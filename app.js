0a26135bf5e63798dbde7110969e9a37c317bd0b
Sumaya-Mohamed <160426864+Sumaya-Mohamed@users.noreply.github.com>
2026-03-11 23:27:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ function updateEnemies() {
   for (let enemy of enemies) {
     if (!enemy.alive) continue;
 
-    enemy.y += 0.15;
+    enemy.y += 0.4;
 
     if (enemy.y + enemy.height >= player.y) {
       gameOver = true;
